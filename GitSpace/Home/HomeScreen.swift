b48e3d951f3fcb67bdc7cacbd3555f3ec65e96b7
Cuma Haznedar <cumahaznedar@gmail.com>
2024-05-04 13:08:03 +0200

    Initial commit

--- GitSpace/Home/HomeScreen.swift
+++ GitSpace/Home/HomeScreen.swift
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class HomeScreen: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
